ae1ca7c9e3b1c5e8f30f805b7ab29b01e802288e
lolzzzyo <lolzzzyo@hotmail.com>
2019-06-05 01:32:08 +0200

    Add files via upload
    added arraylist to reiziger class

--- src/model/Reiziger.java
+++ src/model/Reiziger.java
@@ -1,48 +1,81 @@
 package model;
 
-
 import java.sql.Date;
+import java.util.ArrayList;
 
 public class Reiziger {
-	private int id;
-	private String vletters;
-	private String tussenvoegsel;
+	private int reiziger_id;
+	private String voorletters;
+	private String tussenvoegsel;private                    
 	private String achternaam;
 	private Date gbdatum;
+	private ArrayList<OVChipkaart> kaarten;
 	
-	public Reiziger(int id, String vletters, String tussenvoegsel, String achternaam, Date gbdatum) {
-		this.id = id;
-		this.vletters = vletters;
+	public Reiziger(){
+		
+	}
+	
+	public Reiziger(int reiziger_id, String voorletters, String tussenvoegsel, String achternaam, Date gbdatum){
+		this.reiziger_id = reiziger_id;
+		this.voorletters = voorletters;
 		this.tussenvoegsel = tussenvoegsel;
 		this.achternaam = achternaam;
 		this.gbdatum = gbdatum;
+		this.kaarten = new ArrayList<OVChipkaart>();
 	}
 	
-	public int getID() {
-		return id;
+	public int getReizigerId(){
+		return reiziger_id;
 	}
-	public String getVletters() {
-		return vletters;
+	
+	public void setReizigerId(int reiziger_id){
+		this.reiziger_id = reiziger_id;
 	}
-	public void setVletters(String vletters) {
-		this.vletters = vletters;
+
+	public Date getGbdatum() {
+		return gbdatum;
 	}
-	public String getTussenvoegsel() {
-		return tussenvoegsel;
+
+	public void setGbdatum(Date gbdatum) {
+		this.gbdatum = gbdatum;
 	}
-	public void setTussenvoegsel(String tussenvoegsel) {
-		this.tussenvoegsel = tussenvoegsel;
+	
+	public String toString(){
+		return voorletters + " " + tussenvoegsel + " " + achternaam + " " + gbdatum;
 	}
+
 	public String getAchternaam() {
 		return achternaam;
 	}
+
+	public String getVoorletters() {
+		return voorletters;
+	}
+
+	public void setVoorletters(String voorletters) {
+		this.voorletters = voorletters;
+	}
+
+	public String getTussenvoegsel() {
+		return tussenvoegsel;
+	}
+
+	public void setTussenvoegsel(String tussenvoegsel) {
+		this.tussenvoegsel = tussenvoegsel;
+	}
+
 	public void setAchternaam(String achternaam) {
 		this.achternaam = achternaam;
 	}
-	public Date getGBdatum() {
-		return gbdatum;
+	public Collection<OVChipkaart> getKaarten() {
+	    return kaarten;
 	}
-	public void setGBdatum(Date gbdatum) {
-		this.gbdatum = gbdatum;
-	}
+
+	public void addKaart(OVChipkaart kaart) {
+        if (!getKaarten().contains(kaart)) {
+            getKaarten().add(kaart);
+            
+            kaart.setReiziger(this);
+        }
+    }
 }
